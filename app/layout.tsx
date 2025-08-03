f174df8e6f2d66d3cb23ff7dcf4c38a7f4fae705
ahmed <ahmedsalah998.as@gmail.com>
2025-08-03 17:18:49 +0300

    metaads

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+			<meta name="google-adsense-account" content="ca-pub-2449405226749473" />
+			</head>
 			<body className={fontSans.variable}>{children}</body>
 		</html>
 	);
